bca85eb5e393b96f4f20f6dc0bb03b212b214dbd
Alejo <alejococa23@gmail.com>
2022-08-15 23:39:17 -0300

    mejoras

--- app.js
+++ app.js
@@ -13,19 +13,19 @@ app.use(express.static(__dirname + '/src'))
 
 const puerto = process.env.PORT || 3000;
 
-hbs.registerHelper('obtenerFondo',(equipo, equipostotales)=>{
-  if(equipo.posicion<=4){
-    return "primeros "+equipostotales;
-  }
-  else if(equipo.posicion>equipostotales-4){
-    return "ultimos";
-  }
-})
-
 app.get('/', async (req, res) => {
   const equipos = await Equipo.find({})
-  console.log(equipos.length)
   res.render('index', { equipos })
+})
+
+// HELPER
+hbs.registerHelper('obtenerFondo', (equipo, equipostotales) => {
+  if (equipo.posicion <= 4) {
+    return "primeros " + equipostotales;
+  }
+  else if (equipo.posicion > equipostotales - 4) {
+    return "ultimos";
+  }
 })
 
 async function scraper() {
@@ -36,36 +36,32 @@ async function scraper() {
   let newEquipo = new Equipo()
   for (let i = 0; i < $('tr td').length / 10; i++) {                        // en el for uso $('tr td')/10 porque $('tr td') devuelve todos los datos de la tabla
     newEquipo.nombre = $('tr td').eq(i * 10 + 1).find('span.d-none').text() // y como cada equipo tiene 10 datos al dividir por 10 me da la cantidad de equipos sin la nesesidad de harcodearlo
-    let equipo = await Equipo.findOne({ nombre: newEquipo.nombre })
-    if (equipo == undefined) {
-      newEquipo.posicion = $('tr td').eq(i * 10 + 0).text()
-      newEquipo.escudo = $('tr td').eq(i * 10 + 1).find('.lazy').attr('data-src')
-      newEquipo.partidos_jugados = $('tr td').eq(i * 10 + 2).text()
-      newEquipo.ganados = $('tr td').eq(i * 10 + 3).text()
-      newEquipo.empatados = $('tr td').eq(i * 10 + 4).text()
-      newEquipo.perdidos = $('tr td').eq(i * 10 + 5).text()
-      newEquipo.goles_favor = $('tr td').eq(i * 10 + 6).text()
-      newEquipo.goles_contra = $('tr td').eq(i * 10 + 7).text()
-      newEquipo.diferencia_goles = $('tr td').eq(i * 10 + 8).text()
-      newEquipo.puntos = $('tr td').eq(i * 10 + 9).text()
-      console.log("GUARDADO")
-      await newEquipo.save()
-    } else if (equipo.nombre == newEquipo.nombre) {
-      console.log("ACTUALIZADO")
-      await Equipo.findOneAndUpdate({ nombre: newEquipo.nombre }, {
-        $set: {
-          posicion: newEquipo.posicion,
-          escudo: newEquipo.escudo,
-          partidos_jugados: newEquipo.partidos_jugados,
-          ganados: newEquipo.ganados, empatados: newEquipo.empatados,
-          perdidos: newEquipo.perdidos, goles_favor: newEquipo.goles_favor,
-          goles_contra: newEquipo.goles_contra,
-          diferencia_goles: newEquipo.diferencia_goles,
-          puntos: newEquipo.puntos
-        }
-      })
-    }
-    newEquipo = new Equipo({})
+    newEquipo.posicion = $('tr td').eq(i * 10 + 0).text()
+    newEquipo.escudo = $('tr td').eq(i * 10 + 1).find('.lazy').attr('data-src')
+    newEquipo.partidos_jugados = $('tr td').eq(i * 10 + 2).text()
+    newEquipo.ganados = $('tr td').eq(i * 10 + 3).text()
+    newEquipo.empatados = $('tr td').eq(i * 10 + 4).text()
+    newEquipo.perdidos = $('tr td').eq(i * 10 + 5).text()
+    newEquipo.goles_favor = $('tr td').eq(i * 10 + 6).text()
+    newEquipo.goles_contra = $('tr td').eq(i * 10 + 7).text()
+    newEquipo.diferencia_goles = $('tr td').eq(i * 10 + 8).text()
+    newEquipo.puntos = $('tr td').eq(i * 10 + 9).text()
+    await Equipo.updateOne({ nombre: newEquipo.nombre }, {
+      $set: {
+        nombre: newEquipo.nombre,
+        escudo: newEquipo.escudo,
+        posicion: newEquipo.posicion,
+        puntos: newEquipo.puntos,
+        partidos_jugados: newEquipo.partidos_jugados,
+        ganados: newEquipo.ganados,
+        empatados: newEquipo.empatados,
+        perdidos: newEquipo.perdidos,
+        goles_favor: newEquipo.goles_favor,
+        goles_contra: newEquipo.goles_contra,
+        diferencia_goles: newEquipo.diferencia_goles
+      }
+    }, 
+    { upsert: true })
   }
   //console.log(await Equipo.find()) sacar comentario para poder ver los equipos en la base de datos
 }
